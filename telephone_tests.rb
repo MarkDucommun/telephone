04891734496821b370298c59aad9c0e9afa9fd35
dbc-apprentice <apprentice@devbootcamp.com>
2013-08-27 11:34:27 -0500

    added 2 tests

--- telephone_tests.rb
+++ telephone_tests.rb
@@ -2,7 +2,8 @@ require 'rspec'
 require_relative 'task'
 
 describe Task do
-
+  let(:task) { Task.new("A Title", "A Description") }
+ 
   describe "#initialize" do
     it "should require two arguments" do
       expect { Task.new }.to raise_error
@@ -10,6 +11,18 @@ describe Task do
 
     it "it succeeds with two arguments" do
       expect {Task.new("A Title", "A Description") }.not_to raise_error
+    end
+  end
+
+  describe "#created_at" do
+    it "should have created at" do
+      expect(task).to respond_to(:created_at)
+    end
+  end
+
+  describe "#status" do
+    it "should have status"
+      expect(task).to respond_to(:status)
     end
   end
 end
