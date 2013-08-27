24d36ba95022dc57a39fc00e88bf122c292ad100
Tom K <tk.kowal@gmail.com>
2013-08-27 11:24:08 -0500

    initialize method for task

--- telephone_tests.rb
+++ telephone_tests.rb
@@ -1,15 +1,15 @@
 require 'rspec'
-# require_relative '?'
+require_relative 'task'
 
 describe Task do
-  
+
   describe "#initialize" do
     it "should require two arguments" do
       expect { Task.new }.to raise_error
     end
 
     it "it succeeds with two arguments" do
-      expect {Task.new("A Title", "A Description") }.not_to raise_error 
+      expect {Task.new("A Title", "A Description") }.not_to raise_error
     end
   end
 end
